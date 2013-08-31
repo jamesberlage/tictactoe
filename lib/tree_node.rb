632eae271a79c44c74689a77213f2112b1535605
jamesberlage <james.berlage@gmail.com>
2013-08-31 12:40:44 -0400

    Working TreeNode

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -1,9 +1,27 @@
 class TreeNode
 
-  attr_accessor :value, :parent, :children
+  attr_reader :parent
+  attr_accessor :value, :children
 
   def initialize(value=nil, parent=nil, children=[])
     @value, @parent, @children = value, parent, children
+  end
+
+  def ==(other_node)
+    self.value == other_node.value &&
+    self.parent == other_node.parent &&
+    self.children == other_node.children
+  end
+
+  def parent=(other_node)
+    raise "can't add a parent unless they claim you as their child" if !other_node.children.include?(self)
+
+    @parent = other_node
+  end
+
+  def add_child(other_node)
+    self.children << other_node
+    other_node.parent = self
   end
 
 end
--- spec/tree_node_spec.rb
+++ spec/tree_node_spec.rb
@@ -3,7 +3,7 @@ require 'tree_node'
 
 describe TreeNode do
 
-  subject(:tree_node) { TreeNode.new }
+  subject(:tree_node) { TreeNode.new(13) }
 
   it "has a parent" do
     expect(tree_node.parent).to be_nil
@@ -14,21 +14,65 @@ describe TreeNode do
   end
 
   it "has a value" do
-    expect(tree_node.value).to be_nil
+    expect(tree_node.value).to eq(13)
   end
 
-  it "checks for equivalent nodes"
+  describe "#==" do
+
+    let(:other_node) { double("other_node", value: 13, parent: nil, children: []) }
+
+    it "checks for equivalent nodes" do
+      expect(tree_node == other_node).to be_true
+    end
+
+  end
 
   describe "#parent=" do
+
+    let(:node_without_children) { double("node_without_children", value: 13, parent: nil, children: []) }
+    let(:node_with_children) { double("node_with_children", value: 23, parent: nil, children: [tree_node]) }
+
+    it "sets parent equal to another node" do
+      tree_node.parent = node_with_children
+      expect(tree_node.parent).to eq(node_with_children)
+    end
+
+    it "raises error if the parent has no children" do
+      expect { tree_node.parent = node_without_children }.to raise_error("can't add a parent unless they claim you as their child")
+    end
 
   end
 
   describe "#add_child" do
 
-    it "changes the child's parent to be the current node"
+    let(:child_node) { TreeNode.new(14) }
 
-    it "adds the child to the current node's children"
+    it "changes the child's parent to be the current node" do
+      tree_node.add_child(child_node)
+      expect(child_node.parent).to eq(tree_node)
+    end
+
+    it "adds the child to the current node's children" do
+      tree_node.add_child(child_node)
+      expect(tree_node.children.include?(child_node)).to be_true
+    end
 
   end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
